85193041a48b60956944e118787dafc181e22773
Jake Adams <norepro.github@outlook.com>
2015-06-13 16:05:12 -0700

    do some renaming

--- src/binaryHeap.js
+++ src/binaryHeap.js
@@ -2,31 +2,100 @@ module.exports = function (/*option comparator will be passed here*/) {
   'use strict';
 
   return {
-    collection: [],
+    __collection: [],
+
     size: function size() {
-      return this.collection.length;
+      return this.__collection.length;
     },
-    parentIndex: function (n) {
-      return Math.floor((n + 1) / 2) - 1;
-    },
-    childIndex: function (n) {
-      return {
-        left: (n + 1) * 2 - 1,
-        right: (n + 1) * 2
-      };
-    },
-    hasChildren: function (index){
-      var children = this.childIndex(index);
-      return this.getItem(children.left) != undefined || this.getItem(children.right) != undefined;
-    },
-    getItem: function (index) {
+
+    getItem: function getItem(index) {
       if (index > this.size() - 1) {
         return;
       }
 
-      return this.collection[index];
+      return this.__collection[index];
     },
-    getIndexOfSmallest: function getIndexOfSmallest(subjectIndex, leftIndex, rightIndex) {
+
+    push: function push(item) {
+      this.__collection.push(item);
+      var index = this.size() - 1;
+
+      if (index === 0) {
+        return;
+      }
+
+      this.__heapify(index);
+    },
+
+    shift: function shift() {
+      var firstItem = this.__collection[0];
+
+      var lastItem = this.__collection.pop();
+
+      this.__collection[0] = lastItem;
+
+      this.__siftDown(0);
+
+      return firstItem;
+    },
+
+    __heapify: function __heapify(index) {
+      var me = this.__collection[index],
+        parentIndex = this.__parentIndex(index),
+        parent = this.__collection[parentIndex];
+
+      if (me > parent) {
+        return;
+      }
+
+      this.__collection[parentIndex] = me;
+      this.__collection[index] = parent;
+
+      if (parentIndex > 0) {
+        this.__heapify(parentIndex);
+      }
+    },
+
+    __siftDown: function __siftDown(index) {
+      // get children indexes
+      var children = this.__childIndex(index);
+
+      var childIndex = this.__getIndexOfSmallest(index, children.left, children.right);
+
+      if (index === childIndex) {
+        return;
+      }
+
+      var child = this.__collection[childIndex];
+      var me = this.__collection[index];
+
+      this.__collection[childIndex] = me;
+      this.__collection[index] = child;
+
+      if(this.__hasChildren(childIndex)){
+        this.__siftDown(childIndex);
+      }
+
+      return;
+    },
+
+    __parentIndex: function __parentIndex(index) {
+      return Math.floor((index + 1) / 2) - 1;
+    },
+
+    __childIndex: function __childIndex(index) {
+      return {
+        left: (index + 1) * 2 - 1,
+        right: (index + 1) * 2
+      };
+    },
+
+    __hasChildren: function __hasChildren(index){
+      var children = this.__childIndex(index);
+      return this.getItem(children.left) != undefined || this.getItem(children.right) != undefined;
+    },
+
+    __getIndexOfSmallest: function __getIndexOfSmallest(subjectIndex, leftIndex, rightIndex) {
       var subjectValue = this.getItem(subjectIndex),
         leftValue = this.getItem(leftIndex),
         rightValue = this.getItem(rightIndex);
@@ -43,64 +112,5 @@ module.exports = function (/*option comparator will be passed here*/) {
 
       return smallestChild === leftValue ? leftIndex : rightIndex;
     },
-    push: function push(item) {
-      this.collection.push(item);
-      var index = this.size() - 1;
-
-      if (index === 0) {
-        return;
-      }
-
-      this.bubbleUp(index);
-    },
-    shift: function shift() {
-      var firstItem = this.collection[0];
-
-      var lastItem = this.collection.pop();
-
-      this.collection[0] = lastItem;
-
-      this.siftDown(0);
-
-      return firstItem;
-    },
-    bubbleUp: function (index) {
-      var me = this.collection[index],
-        parentIndex = this.parentIndex(index),
-        parent = this.collection[parentIndex];
-
-      if (me > parent) {
-        return;
-      }
-
-      this.collection[parentIndex] = me;
-      this.collection[index] = parent;
-
-      if (parentIndex > 0) {
-        this.bubbleUp(parentIndex);
-      }
-    },
-    siftDown: function (index) {
-      // get children indexes
-      var children = this.childIndex(index);
-
-      var childIndex = this.getIndexOfSmallest(index, children.left, children.right);
-
-      if (index === childIndex) {
-        return;
-      }
-
-      var child = this.collection[childIndex];
-      var me = this.collection[index];
-
-      this.collection[childIndex] = me;
-      this.collection[index] = child;
-
-      if(this.hasChildren(childIndex)){
-        this.siftDown(childIndex);
-      }
-
-      return;
-    }
   }
 };
